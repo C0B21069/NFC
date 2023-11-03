7f13df503a7ecbc495678e01a4f01d8ff1db7eea
C0B21069 <c0b2106965@edu.teu.ac.jp>
2023-11-03 16:37:34 +0900

    second commit

--- temp.py
+++ temp.py
@@ -1,16 +1,21 @@
 import time
-
+import requests
 import busio
 import board
 
 import adafruit_amg88xx
-def temp():
+
+while True:
+    url = "https://maker.ifttt.com/trigger/temp/with/key/cNIQxlL1O5HfyMf7-WUVdh"
     i2c = busio.I2C(board.SCL, board.SDA)
     sensor = adafruit_amg88xx.AMG88XX(i2c, addr=0x68)
     time.sleep(0.2)
-    return sensor.pixels
+    temp = [sum(temps) for temps in sensor.pixels]
+    total_temp = sum(temp)//64
+    payload = {"value1":str(total_temp),"value2":str("nothing"),"value3":str("nothing")}
+    response = requests.post(url,data=payload)
+    print(total_temp)
+    time.sleep(10)
 
-if __name__ == '__main__':
-    temp()
 
 
